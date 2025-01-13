18cafd86b9fc0aa5c44eaad8cba472ee0a19e51a
baekminjin <bhs3392@naver.com>
2025-01-13 14:13:25 +0900

    SecurityConfig , Test -> WithUserDetails

--- src/test/java/com/ll/springsecurity/restByTdd/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/ll/springsecurity/restByTdd/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -83,6 +84,7 @@ public class ApiV1PostControllerTest {
     }
 
     @Test
+    @WithUserDetails("user")
     @DisplayName("글 작성")
     void t3() throws Exception {
         Member actor = memberService.findByUsername("user1").get();
