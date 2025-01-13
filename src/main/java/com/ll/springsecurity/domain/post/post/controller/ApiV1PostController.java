1a2cc3743abae73ff5be2a091e34a54bf44d0d5c
baekminjin <bhs3392@naver.com>
2025-01-13 14:37:35 +0900

    UserDetails

--- src/main/java/com/ll/springsecurity/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/springsecurity/domain/post/post/controller/ApiV1PostController.java
@@ -12,6 +12,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -85,13 +88,13 @@ public class ApiV1PostController {
     @Transactional
     public RsData<PostWithContentDto> write(
             @RequestBody @Valid PostWriteReqBody reqBody,
-             Principal principal
+            @AuthenticationPrincipal UserDetails user
     ) {
         Member actor = rq.checkAuthentication();
 
 
-        if (principal != null) {
-            actor = rq.getActorByUsername(principal.getName());
+        if (user != null) {
+            actor = rq.getActorByUsername(user.getUsername());
         }
 
         Post post = postService.write(
